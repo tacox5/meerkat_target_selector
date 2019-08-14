b1a5275c20829bdb987fa89fa79523598daf0173
Tyler Cox <tyler@airbears2-10-142-42-27.airbears2.1918.berkeley.edu>
2019-08-14 14:01:42 -0700

    Add mysqlconnector to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ requires = [
     "numpy==1.16.2",
     "credentials==1.1",
     "python_dateutil==2.8.0",
-    "PyYAML==5.1.2"
+    "PyYAML==5.1.2",
+    "mysqlclient==1.4.4"
 ]
 
 setuptools.setup(
